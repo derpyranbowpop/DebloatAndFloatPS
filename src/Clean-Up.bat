383af19f61b95592c1ae94f04895fdf278064663
derpyranbowpop <55472010+derpyranbowpop@users.noreply.github.com>
2023-08-17 18:49:02 +1000

    Renamed delete to clean-up

---
+++ src/Clean-Up.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Deletes scripts from temp folder 
+rmdir C:\Temp\DebloatAndFloat\bin /Q /S
+echo Compleated Clean-Up 
+pause
--- src/Delete.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Deletes scripts from temp folder 
-rmdir C:\Temp\DebloatAndFloat\bin /Q /S
